1c6d724b66806e3a1fb620cb4507f4166fc3249d
Chukeze <enile12@gmail.com>
2023-02-24 18:13:01 -0600

    Update The styling and fix form error bugs

--- fem-interactive-card-details/main.js
+++ fem-interactive-card-details/main.js
@@ -1,5 +1,5 @@
 import './style.css';
-//import { cardManipulation } from './functionality.js';
+
 const number = new RegExp(/^d+$/);
 document.querySelector('#app').innerHTML = `
   <aside>
@@ -23,33 +23,28 @@ document.querySelector('#app').innerHTML = `
       </div>
   </aside>
   <main role="main">
-    <section class="padding-block-700">
+    <section class="padding-block-700 form">
       <div class="container">
         <form class="even-rows" novalidate>
           <div class="inputs">
             <label for="name"> Cardholder Name</label>
             <input type="text" id="name" name="name" placeholder="e.g. Jane Appleseed"  required/>
-            <p class="form-error" data-error="true></p>
+            <p class="form-error" data-error="true"></p>
           </div>
           <div class="inputs">
             <label for="number"> Card Number</label>
-            <input type="text" pattern="[0-9, \s]+" id="card-number" name="number"  placeholder="e.g. 1234 5678 9128 0000" required/>
-            <p class="form-error" data-error="true></p>
+            <input type="text" pattern="[0-9, /\s]+" id="card-number" name="number"  placeholder="e.g. 1234 5678 9128 0000" required/>
+            <p class="form-error" data-error="true"></p>
           </div>
-          <div class="odd-columns">
-            <div class="inputs">
-              <label for="month"> Exp. Date <span> (MM/YY)</span></label>
-              <div class="flex">
-                <input type="text" id="month" name="month" placeholder="MM" pattern="[0-9]+{1,2}" required>
-                <input type="text" id="year" name="year" placeholder="YY" pattern="[0-9]+{1,2}" required>
-              </div>
-              <p class="form-error" data-error="true></p>
-            </div>
-            <div class="inputs">
-              <label for="cvc"> CVC </label>
-              <input type="text" id="cvc" name="cvc" min-length="3" maxlength="4" placeholder="e.g. 123" pattern="[0-9]{4}" required>
-            </div>
-          </div>
+          <fieldset class="inputs odd-columns">
+            <label for="month"> Exp. Date <span> (MM/YY)</span></label>
+            <input type="text" id="month" name="month" placeholder="MM" pattern="[0-9]+" required/>
+            <input type="text" id="year" name="year" placeholder="YY" pattern="[0-9]+" required/>
+            <p class="form-error" data-error="true"></p>
+            <label for="cvc"> CVC </label>
+            <input type="text" id="cvc" name="cvc" min-length="3" maxlength="4" placeholder="e.g. 123" pattern="[0-9]{3}" required/>
+            <p class="form-error" data-error="true"></p>
+          </fieldset>
           <button type="submit" id="confirm"> Confirm</button>
         </form>
       </div>
@@ -72,7 +67,8 @@ document.querySelector('#app').innerHTML = `
 `
 const domElements = {
   form: document.querySelector('form'),
-  success: document.querySelector('.success'),
+  view: document.querySelector('.form'),
+  success: document.querySelector('.hidden'),
   reset: document.querySelector('#reset-button'),
   cards: {
     name: document.querySelector('.owner'),
@@ -94,15 +90,15 @@ const domElements = {
 //pattern = "[0-9]{1,2}"
 //pattern = "[0-9]{4}"
 //[0-9, *\s]+
-const getErrorElement = (el=HTMLElement) => {
-  const next = el.prototype.nextElementSibling;
-  if( next?.dataset.error) return next;
+const getErrorElement = (el) => {
+  const next = el.nextElementSibling;
+  if (next === null || next?.dataset.error) return next;
   return getErrorElement(next)
 };
 
 const errorMssg = {
   required: "Can't be blank",
-  pattern:"Value missing, number only",
+  pattern:"Wrong Format, number only",
 };
 
 domElements.form?.addEventListener('submit', e => {
@@ -112,22 +108,26 @@ domElements.form?.addEventListener('submit', e => {
   const fields = target.querySelectorAll('input');
   let isValid = true;
   fields.forEach(field => {
-    const inputElements = HTMLInputElement;
-    field = inputElements.prototype.validity;
-    let validation = field;
-    const validate = validation;
+    const validity = field.validity;
     const errorElement = getErrorElement(field);
-    if(validate.valid){
+    //const inputElements = HTMLInputElement;
+    //field = inputElements.prototype.validity;
+    //let validation = field;
+    //const validate = validation;
+    //const errorElement = getErrorElement(field);
+    if(validity.valid){
       field.classList.remove('error');
       errorElement.textContent = '';
       return;
     }
+    console.log(field.name, validity);
     isValid = false;
     field.classList.add('error');
-    errorElement.textContent = validate.valueMissing ? errorMssg.required : errorMssg.pattern;
+    errorElement.textContent = validity.valueMissing ? errorMssg.required : errorMssg.pattern;
   });
   if( isValid) {
     domElements.form?.classList.add('hidden');
+    domElements.view?.classList.add('hidden');
     domElements.success?.classList.remove('hidden');
   }
 
@@ -143,7 +143,13 @@ const updatePreview = () => {
 
 domElements.reset?.addEventListener('click', () => {
   domElements.form?.classList.remove('hidden');
-  domElements.success?.classList.remove('hidden');
+  domElements.view?.classList.remove('hidden');
+  domElements.success?.classList.add('hidden');
+  domElements.cards.name.textContent = 'Jane Appleseeds';
+  domElements.cards.number.textContent = '0000 0000 0000 0000';
+  domElements.cards.cvc.textContent = '000';
+  domElements.cards.date.month.textContent = '00';
+  domElements.cards.date.year.textContent = '00';
   domElements.form?.reset();
 });
 
